8df0b296caefd3ad3faf8b77d837b38338f2c1f6
Tanguy Genthon <72631088+TanguyGen@users.noreply.github.com>
2025-03-20 11:16:01 +0100

    Add external data and R scripts

--- app.R
+++ app.R
@@ -2,13 +2,15 @@ library(shiny)
 library(leaflet)
 library(sf)
 library(tidyr)
+library(dplyr)
 library(DT)
 library(ggplot2)
 library(gridExtra)
 library(icesTAF)
 library(tibble)
 library(shinycssloaders)
-library(shinyjs)
+library(data.table)
+
 
 # Load necessary data
 ecoregions <- readRDS("Data/Ecoregions.RData")
@@ -19,7 +21,6 @@ load(url(url_github))
 category_choices <- unique(info$category)
 category_choices <- category_choices[!is.na(category_choices)]
 
-# Define UI
 ui <- fluidPage(
   tags$head(includeCSS("www/styles.css")),
   tags$head(
@@ -31,7 +32,12 @@ ui <- fluidPage(
       #graphs-container {
         overflow-y: auto;
         border: 1px solid #ccc;
-        transition: height 0.5s ease;  /* Smooth transition when resizing */
+        transition: height 0.5s ease;
+        height: auto; /* Use auto for dynamic height */
+      }
+      #Graphs {
+        height: 100vh; /* Set initial height to view height */
+        overflow-y: auto; /* Enable vertical scrolling */
       }
     ")
   ),
@@ -42,7 +48,7 @@ ui <- fluidPage(
       h1(
         img(
           width = 200,
-          src = "ICES_logo.png", # Ensure the image is located in www/ directory
+          src = "ICES_logo.png",
           class = "topleft"
         )
       ),
@@ -72,6 +78,7 @@ ui <- fluidPage(
 )
 
 
+
 server <- function(input, output, session) {
   # Track original indices when filtering for selected categories
   filtered_data <- reactive({
@@ -90,7 +97,7 @@ server <- function(input, output, session) {
     info_filtered %>% select(selectable, FullName, unit, category)  
   })
   
-  rv <- reactiveValues(DataVariables = NULL, original_indices = NULL)
+  rv <- reactiveValues(DataVariables = NULL, original_indices = NULL, tabsCreated = FALSE)
   
   output$lastUpdate <- renderText({
     paste("Last updated on:", extract_github_commit_date())
@@ -107,7 +114,7 @@ server <- function(input, output, session) {
       filtered_data(),
       escape = FALSE,
       options = list(
-        pageLength = 10,
+        pageLength = 20,
         dom = 'Bfrtip',
         autoWidth = TRUE,
         ordering = FALSE,
@@ -153,38 +160,45 @@ server <- function(input, output, session) {
     )
   })
   
+  # Modifying the observeEvent function for the 'continue' button
   observeEvent(input$continue, {
     req(input$selected_ids)
     
     # Convert selected row indices in DT table to original indices stored in rv
     selected_indices <- rv$original_indices[as.numeric(input$selected_ids) + 1]
-    
     rv$DataVariables <- table.all[selected_indices]
     
-    appendTab(inputId = "menu",
-              tabPanel(
-                "Graphs",
-                plotOutput("Graphs")
-              )
-    )
-    appendTab(inputId = "menu",
-              tabPanel(
-                "Download",
-                sidebarLayout(
-                  sidebarPanel(
-                    selectInput("year_selector", "Select Year:", choices = seq(from = 1980, to = 2023, by = 1), selected = 2023)
-                  ),
-                  mainPanel(
-                    fluidRow(
-                      downloadButton("ZIP", label = "Download ZIP file")
+    # Check if tabs have not been created yet
+    if (!rv$tabsCreated) {
+      rv$tabsCreated <- TRUE
+      
+      appendTab(inputId = "menu",
+                tabPanel(
+                  "Graphs",
+                  fluidRow(
+                    column(12, plotOutput("Graphs", height = "800px"))
+                  )
+                )
+      )
+      appendTab(inputId = "menu",
+                tabPanel(
+                  "Download",
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput("startyear", "Select a start year:", choices = seq(from = 1980, to = 2023, by = 1), selected = 2023),
+                      br(),
+                      selectInput("endyear", "Select an end year:", choices = seq(from = 1980, to = 2023, by = 1), selected = 2023)
+                    ),
+                    mainPanel(
+                      fluidRow(downloadButton("CSV", label = "Download CSV file"))
                     )
                   )
                 )
-              )
-    )
-    
-    updateTabsetPanel(session, "menu", selected = "Graphs")
+      )
+      updateTabsetPanel(session, "menu", selected = "Graphs")
+    }
   })
+  
   
   output$Graphs <- renderPlot({
     selected_data <- rv$DataVariables
@@ -219,17 +233,74 @@ server <- function(input, output, session) {
                   axis.text = element_text(size = 12))
         }
       }
-      
-      gridExtra::grid.arrange(grobs = plotlist, ncol = 2)
+      if (!is.null(plotlist) && length(plotlist) > 0) {
+        n <- length(plotlist)
+        numrow <- ceiling(n / 2)  # Calculate the number of rows needed
+        
+        # Ensure plotlist is a list of ggplots or grobs
+        if (all(sapply(plotlist, inherits, "ggplot"))) {
+          do.call(gridExtra::grid.arrange, c(plotlist, nrow = numrow))
+        } else {
+          print("Plotlist does not contain valid ggplot objects.")
+        }
+      }
     }
+  }, height = function() {
+    length(rv$DataVariables) * 400  # Adjust height dynamically based on number of plots
   })
   
-  output$Download <- downloadHandler(
-    filename = paste0("Data_", format(Sys.time(), "%s"), ".zip"),
+  output$CSV <- downloadHandler(
+    filename = function() {
+      paste0("Data_", format(Sys.time(), "%s"), ".csv")
+    },
     content = function(file) {
-      zip(file, files = c(), extras = '-j')
+      # Retrieve selected years
+      startyear <- as.numeric(input$startyear)
+      endyear <- as.numeric(input$endyear)
+      
+      # Validate year range
+      if (startyear > endyear) {
+        # Show error modal dialog and prevent download
+        shiny::showModal(shiny::modalDialog(
+          title = "Invalid Year Range",
+          paste("The start year (", startyear, ") cannot be greater than the end year (", endyear, ")."),
+          easyClose = TRUE,
+          footer = NULL
+        ))
+        return()  # Exit without downloading
+      }
+      
+      # Proceed with data modification and CSV creation if year range is valid
+      selected_data <- rv$DataVariables
+      
+      # Initialize an empty list to store modified data.frames
+      modified_data <- list()
+      
+      for (i in seq_along(selected_data)) {
+        data_item <- selected_data[[i]]
+        
+        # Ensure the data_item is a data.table
+        if (!is.data.table(data_item)) {
+          data_item <- as.data.table(data_item)
+        }
+        
+        # Filter data based on year range
+        data_item <- data_item %>%
+          filter(year >= startyear & year <= endyear)
+        
+        # Add a column to the data.table
+        data_item[, TableName := info$FullName[rv$original_indices[as.numeric(input$selected_ids)[i] + 1]]]
+        
+        # Append the modified data.table to the list
+        modified_data[[i]] <- data_item
+      }
+      
+      # Bind all data.tables row-wise and write to CSV
+      Outputtable <- rbindlist(modified_data)
+      write.csv(Outputtable, file)
     }
   )
+  
 }
 
 shinyApp(ui = ui, server = server)
